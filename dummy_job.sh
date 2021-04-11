140ec4a5b2458b07c02c60b7dd5d63dd1bf7c3d5
shahine <shahine96@gmail.com>
2021-04-11 12:27:46 +0100

    fixed couple of device compatibility issues

--- dummy_job.sh
+++ dummy_job.sh
@@ -9,11 +9,12 @@
 #SBATCH --error=/data/ziz/not-backed-up/bouabid/tmp/slurm-%A_%a.o
 
 #pyenv activate deconditioning
-# python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/exact_cme_process.yaml --o=sandbox/boo_exact_wo --n_epochs=2
-# python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/exact_cme_process_indiv_noise.yaml --o=sandbox/boo_exact_w --n_epochs=2
-# python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/variational_cme_process.yaml --o=sandbox/boo_var_wo --n_epochs=2
-# python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/variational_cme_process_indiv_noise.yaml --o=sandbox/boo_var_w --n_epochs=2
-# python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/vbagg.yaml --o=sandbox/boo_vbagg --n_epochs=2
+python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/exact_cme_process.yaml --o=sandbox/boo_exact_wo --n_epochs=2
+python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/exact_cme_process_indiv_noise.yaml --o=sandbox/boo_exact_w --n_epochs=2
+python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/variational_cme_process.yaml --o=sandbox/boo_var_wo --n_epochs=2
+python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/variational_cme_process_indiv_noise.yaml --o=sandbox/boo_var_w --n_epochs=2
+python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/vbagg.yaml --o=sandbox/boo_vbagg --n_epochs=2
+python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/gp_regression.yaml --o=sandbox/boo_gp_regression --n_epochs=2
 
 # python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/exact_cme_process.yaml --o=sandbox/sandbox/exact_wo_noise --plot
 # python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/exact_cme_process_indiv_noise.yaml --o=sandbox/sandbox/exact_w_noise --plot
@@ -22,8 +23,8 @@
 # python experiments/swiss_roll/run_experiment.py --cfg=experiments/swiss_roll/config/vbagg.yaml --o=sandbox/sandbox/vbagg --plot
 
 
-VARIATIONAL_CME_INDIV_NOISE_CFG=experiments/swiss_roll/config/variational_cme_process_indiv_noise.yaml
-VARIATIONAL_CME_INDIV_NOISE_OUTDIR=sandbox/sandbox/var_w_noise_sweep/
+# VARIATIONAL_CME_INDIV_NOISE_CFG=experiments/swiss_roll/config/variational_cme_process_indiv_noise.yaml
+# VARIATIONAL_CME_INDIV_NOISE_OUTDIR=sandbox/sandbox/var_w_noise_sweep/
 
 # # Variational CME w individuals noise
 # for lbda in 0.00001 0.0001 0.001 0.01 0.1;
@@ -40,11 +41,11 @@ VARIATIONAL_CME_INDIV_NOISE_OUTDIR=sandbox/sandbox/var_w_noise_sweep/
 #   --n_epochs=300 --beta=$beta
 # done
 
-for lr in 0.0001 0.001 0.01 0.1 1;
-do
-  output_dir="lr_"$lr
-  python experiments/swiss_roll/run_experiment.py --cfg=$VARIATIONAL_CME_INDIV_NOISE_CFG --o=$VARIATIONAL_CME_INDIV_NOISE_OUTDIR/$output_dir \
-  --n_epochs=300 --lr=$lr
-done
+# for lr in 0.0001 0.001 0.01 0.1 1;
+# do
+#   output_dir="lr_"$lr
+#   python experiments/swiss_roll/run_experiment.py --cfg=$VARIATIONAL_CME_INDIV_NOISE_CFG --o=$VARIATIONAL_CME_INDIV_NOISE_OUTDIR/$output_dir \
+#   --n_epochs=300 --lr=$lr
+# done
 
 echo "Job Completed"
