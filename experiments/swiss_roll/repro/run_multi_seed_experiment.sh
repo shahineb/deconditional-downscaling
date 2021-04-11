3e45e592971bb9abf5d2f966a40547b78559af7e
shahine <shahine96@gmail.com>
2021-04-11 18:59:08 +0100

    reduced noise

--- experiments/swiss_roll/repro/run_multi_seed_experiment.sh
+++ experiments/swiss_roll/repro/run_multi_seed_experiment.sh
@@ -5,7 +5,7 @@ VARIATIONAL_CME_CFG=experiments/swiss_roll/config/variational_cme_process.yaml
 VARIATIONAL_CME_INDIV_NOISE_CFG=experiments/swiss_roll/config/variational_cme_process_indiv_noise.yaml
 VBAGG_CFG=experiments/swiss_roll/config/vbagg.yaml
 GP_REGRESSION_CFG=experiments/swiss_roll/config/gp_regression.yaml
-LINEAR_INTERPOLATION_CFG=experiments/swiss_roll/config/linear_interpolation.yaml
+#LINEAR_INTERPOLATION_CFG=experiments/swiss_roll/config/linear_interpolation.yaml
 
 # Define output directories path variables
 EXACT_CME_OUTDIR=experiments/swiss_roll/data/experiment_outputs/seeds/exact_cme_process
@@ -15,7 +15,7 @@ VARIATIONAL_CME_INDIV_NOISE_OUTDIR=experiments/swiss_roll/data/experiment_output
 RAW_VBAGG_OUTDIR=experiments/swiss_roll/data/experiment_outputs/seeds/vbagg_beta_1
 VBAGG_OUTDIR=experiments/swiss_roll/data/experiment_outputs/seeds/vbagg
 GP_REGRESSION_OUTDIR=experiements/swiss_roll/data/experiment_outputs/seeds/gp_regression
-LINEAR_INTERPOLATION_OUTDIR=experiments/swiss_roll/data/experiment_outputs/seeds/linear_interpolation
+#LINEAR_INTERPOLATION_OUTDIR=experiments/swiss_roll/data/experiment_outputs/seeds/linear_interpolation
 
 # Run experiments for multiple seeds
 for SEED in 3 5 7 13 15 19 23 31 37 41 42 59 61 67 71 73 79 83 97 101 ;
@@ -27,6 +27,6 @@ do
   python experiments/swiss_roll/run_experiment.py --seed=$SEED --cfg=$VARIATIONAL_CME_INDIV_NOISE_CFG --o=$VARIATIONAL_CME_INDIV_NOISE_OUTDIR/$DIRNAME
   python experiments/swiss_roll/run_experiment.py --seed=$SEED --cfg=$VBAGG_CFG --o=$RAW_VBAGG_OUTDIR/$DIRNAME --beta=1
   python experiments/swiss_roll/run_experiment.py --seed=$SEED --cfg=$VBAGG_CFG --o=$VBAGG_OUTDIR/$DIRNAME
-  python experiemnts/swiss_roll/run_experiment.py --seed=$SEED --cfg=$GP_REGRESSION_CFG --o=$GP_REGRESSION_OUTDIR/$DIRNAME
-  python experiments/swiss_roll/run_experiment.py --seed=$SEED --cfg=$LINEAR_INTERPOLATION_CFG --o=$LINEAR_INTERPOLATION_OUTDIR/$DIRNAME
+  python experiments/swiss_roll/run_experiment.py --seed=$SEED --cfg=$GP_REGRESSION_CFG --o=$GP_REGRESSION_OUTDIR/$DIRNAME
+  #python experiments/swiss_roll/run_experiment.py --seed=$SEED --cfg=$LINEAR_INTERPOLATION_CFG --o=$LINEAR_INTERPOLATION_OUTDIR/$DIRNAME
 done
